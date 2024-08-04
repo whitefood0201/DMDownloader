6b260bfbc4d9f68f5c78b61eb178ffd77e070de7
whitefood0201@gmail.com <sakura.inis123@gmail.com>
2024-08-04 00:15:36 -0400

    修复下载

--- dmdownloader/downloader/gui.py
+++ dmdownloader/downloader/gui.py
@@ -246,8 +246,8 @@ class AnimeFrame(ttk.Frame):
             epid = ep["id"]
             title = ep["title"]
             ttk.Label(master=fra, text=title, width=50, style="ep_title.TLabel").pack(side="left", padx=10)
-            def handler(event):
-                self.download(event, epid, title, site)
+            def handler(event, id=epid, title=title, site=site):
+                self.download(event, id, title, site)
             btn = ttk.Button(master=fra, text="下载", width=6, style="ep_button.TButton")
             btn.bind("<1>", handler)
             btn.pack()
